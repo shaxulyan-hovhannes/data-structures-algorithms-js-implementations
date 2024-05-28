4d3716f5d1a4aea18b745737a0ef91d04fc1fe56
Hovhannes <shaxulyan87@gmail.com>
2024-05-28 18:34:22 +0400

    Get even numbers recursively

--- algorithms/even-numbers-in-array.js
+++ algorithms/even-numbers-in-array.js
@@ -1,1 +1,14 @@
 // Using recursion, write a function that takes an array of numbers and returns a new array containing only the even numbers.
+
+const eventNumbersRecursive = (arr = []) => {
+  if (!arr.length) return [];
+
+  const firstItem = arr[0];
+  const restItems = arr.slice(1);
+
+  if (firstItem % 2 === 0) {
+    return [firstItem].concat(eventNumbersRecursive(restItems));
+  } else {
+    return eventNumbersRecursive(restItems);
+  }
+};
